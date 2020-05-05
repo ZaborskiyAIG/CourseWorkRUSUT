0337b141924d87fcbd192a5764d45d0b66d7395d
ZaborskiyAIG <zaborskiyaig@gmail.ru>
2020-05-05 17:11:43 +0700

    .

--- src/main/java/com/CourseWorkRusut/controller/admin/LibraryControllerAdmin.java
+++ src/main/java/com/CourseWorkRusut/controller/admin/LibraryControllerAdmin.java
@@ -32,22 +32,23 @@ public class LibraryControllerAdmin {
     @PostMapping(value = "/library",produces = "application/pdf")
     public ResponseEntity<InputStreamResource> addLibrary(@RequestParam MultipartFile file) throws IOException {
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.parseMediaType("application/pdf"));
-        headers.add("Access-Control-Allow-Origin", "*");
-        headers.add("Access-Control-Allow-Methods", "GET, POST, PUT");
-        headers.add("Access-Control-Allow-Headers", "Content-Type");
-        headers.add("Content-Disposition", "filename=" + file.getOriginalFilename());
-        headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
-        headers.add("Pragma", "no-cache");
-        headers.add("Expires", "0");
+//        HttpHeaders headers = new HttpHeaders();
+//        headers.setContentType(MediaType.parseMediaType("application/pdf"));
+//        headers.add("Access-Control-Allow-Origin", "*");
+//        headers.add("Access-Control-Allow-Methods", "GET, POST, PUT");
+//        headers.add("Access-Control-Allow-Headers", "Content-Type");
+//        headers.add("Content-Disposition", "filename=" + file.getOriginalFilename());
+//        headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
+//        headers.add("Pragma", "no-cache");
+//        headers.add("Expires", "0");
+//
+//
+//        System.out.println(file.getContentType());
+//        ResponseEntity<InputStreamResource> response = new ResponseEntity<InputStreamResource>(
+//                new InputStreamResource(file.getInputStream()), headers, HttpStatus.OK);
 
-
-        System.out.println(file.getContentType());
-        ResponseEntity<InputStreamResource> response = new ResponseEntity<InputStreamResource>(
-                new InputStreamResource(file.getInputStream()), headers, HttpStatus.OK);
-
-        return response;
+        return null;
+     //   return response;
         // return new ResponseEntity( HttpStatus.OK);
     }
 
