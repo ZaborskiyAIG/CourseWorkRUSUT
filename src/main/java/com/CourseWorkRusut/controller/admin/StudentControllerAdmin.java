240c57296af6095480f4fa18658dde0562660f92
ZaborskiyAIG <zaborskiyaig@gmail.ru>
2020-05-16 12:27:42 +0700

    .

--- src/main/java/com/CourseWorkRusut/controller/admin/StudentControllerAdmin.java
+++ src/main/java/com/CourseWorkRusut/controller/admin/StudentControllerAdmin.java
@@ -84,7 +84,7 @@ public class StudentControllerAdmin {
     @GetMapping(value = "/place_practice")
     public ResponseEntity<Map> getPlacePractice(@RequestParam(value = "offset", defaultValue = "0" )String offset){
         Map<String, Object> map = new HashMap<>();
-        map.put("count",internshipService.counterPlace());
+        map.put("amount",internshipService.counterPlace());
         map.put("content",internshipService.getAllPlace(offset));
         return new ResponseEntity<>(map, HttpStatus.OK);
     }
